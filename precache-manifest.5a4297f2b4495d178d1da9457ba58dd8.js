cad2e3e67afe89bf2956e11d54b28530ec544590
Mary-Tyler-Moore <46167401+Mary-Tyler-Moore@users.noreply.github.com>
2019-05-14 07:40:31 -0400

    Add files via upload

---
+++ precache-manifest.5a4297f2b4495d178d1da9457ba58dd8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17e5fb3f52061ec079c3",
+    "url": "vendors.3ef59a3243270d2fdfb2.bundle.js"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "sw.js"
+  },
+  {
+    "revision": "bf38d0c9760162342f12cd9b44fbf4ef",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "253af49c31048cccbd50",
+    "url": "main.773c785f36286e9a44d7.bundle.js"
+  },
+  {
+    "revision": "1fa6cb602c246c449a34bd32c8539b10",
+    "url": "index.html"
+  },
+  {
+    "revision": "8bdcc486cda9b423f50e886f2ddb6604",
+    "url": "icons/icons-512.png"
+  },
+  {
+    "revision": "2ffbc23293ee8a797bc61e9c02534206",
+    "url": "icons/icons-192.png"
+  },
+  {
+    "revision": "57040e5677322118f6d56a1d9e43c5c6",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "b887c333d5ec8274b78d0851e7b3337b",
+    "url": "assets/img/phaser-logo.png"
+  }
+]);
